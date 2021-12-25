3adff2cade40f90d978c3465ef70c20763e352a3
Anisha Gupta <anishagupta@Anishas-MacBook-Pro.local>
2021-12-25 21:12:46 +0530

    Added-- linkedin profile link

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,9 @@
 <template>
   <h3>Task Tracker's Version 1.0.0</h3>
-  <h4>Created By Anisha Gupta</h4>
+  <h4>
+    Created By
+    <a href="https://www.linkedin.com/feed/" target="_blank">Anisha Gupta</a>
+  </h4>
   <router-link to="/">Go Back</router-link>
 </template>
 
