c607938112cc24c842490f4ffd837bf8d6832f53
Anisha Gupta <anishagupta@Anishas-MacBook-Pro.local>
2021-12-25 18:30:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Task Tracker using vue.js
+
+> This is the project from the YouTube crash course
+## Need to run the JSON-Server backend (http://localhost:5000)
+
+```npm run backend
+```
+## Run the Vue dev server (http://localhost:8080)
+
+```npm run serve
+```
---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <h3>Task Tracker's Version 1.0.0</h3>
+  <h4>Created By Anisha Gupta</h4>
+  <router-link to="/">Go Back</router-link>
+</template>
+
+<script>
+export default {
+  name: "About",
+  inheritAttrs: false, // disable 'non-props' warning
+};
+</script>
